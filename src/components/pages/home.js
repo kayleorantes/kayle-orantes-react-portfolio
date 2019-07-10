004a96bc50a675872b888103283acc81bdd8d58d
kayleorantes <kayle.orantes1@gmail.com>
2019-07-10 10:02:41 -0600

    Initial commit

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,10 @@
 import React from "react";
 import PortfolioContainer from "../portfolio/portfolio-container";
 
-export default function(){
-    return(
-        <div>
-            <h2>Home</h2>
-            <PortfolioContainer/>
-        </div>
-    );
+export default function() {
+  return (
+    <div>
+      <PortfolioContainer />
+    </div>
+  );
 }
