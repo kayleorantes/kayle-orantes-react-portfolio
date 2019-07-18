ec2827b299b9d985cf3aff1dc39979d7e1ee9488
kayleorantes <kayle.orantes1@gmail.com>
2019-07-18 10:11:36 -0600

    Initial commit

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,33 +1,35 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
-export default class PortfolioDetail extends Component{
+export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
 
-    this.state ={
+    this.state = {
       portfolioItem: {}
-    }
+    };
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.getPortfolioItem();
   }
 
-  getPortfolioItem(){
-    axios.get(`https://kayleorantes.devcamp.space/portfolio/portfolio_items/${
-      this.props.match.params.slug}
-      `, 
-      {withCredentials: true}
-    )
-      .then(response =>{
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://kayleorantes.devcamp.space/portfolio/portfolio_items/${
+          this.props.match.params.slug
+        }`,
+        { withCredentials: true }
+      )
+      .then(response => {
         this.setState({
           portfolioItem: response.data.portfolio_item
-        })
-    })
+        });
+      })
       .catch(error => {
-        console.log("getportfolioitem", error);
-    });
+        console.log("getportfolioitem error", error);
+      });
   }
 
   render() {
@@ -41,11 +43,34 @@ export default class PortfolioDetail extends Component{
       url
     } = this.state.portfolioItem;
 
-    return(
-      <div>
-        <h2>{name}</h2>
-        <p>{description}</p>
+    const bannerStyles={
+      backgroundImage: "url(" + banner_image_url + ")",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center"
+    };
+
+    const logoStyles={
+      width: "200px",
+      height: "150px"
+    }
+
+    return (
+      <div className="portfolio-detail-wrapper">
+        <div className="banner" style={bannerStyles}>
+          <img src={logo_url} style={logoStyles}/>
+        </div>
+
+        <div className="portfolio-detail-description-wrapper">
+          <div className="description">{description}</div>
+        </div>
+
+        <div className="bottom-content-wrapper">
+          <a href={url} className="site-link" target="_blank">
+            Visit {name}
+          </a>
+        </div>
       </div>
     );
-  } 
+  }
 }
